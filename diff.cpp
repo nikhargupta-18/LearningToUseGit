8342342ff13219a5079dd3d374a0f94d45a86136
nikhargupta-18 <nikharg1809@gmail.com>
2025-07-21 00:03:35 +0530

    initial commit

---
+++ diff.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char skills [1];
+    skills [0]= 'c';
+    skills [1]= 'g';
+     return 0;
+}
--- index.cpp
+++ index.cpp
@@ -1,4 +1,7 @@
 // git init --> track changes in the file
 //git remote add origin link --> adds the folder to git remote
-// git branch -m name --> changes the branch name to main
+// git remote -v --> to verify remote
+// git branch --> to check branch
+// git branch -m name --> changes the branch name to main 
+// default name master hota hai but apn main rkhna prefer krte hai
 // git push -u origin main --> sets upstream
